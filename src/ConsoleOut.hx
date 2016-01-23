28de8a995d64c228bf738f332e9d2f1eb527a264
Richard McHale <legionsofbob@gmail.com>
2016-01-23 16:14:48 +0000

    Initial commit

---
+++ src/ConsoleOut.hx
@@ -0,0 +1,84 @@
+import neko.Lib;
+
+enum Color
+{
+	Black;
+	Red;
+	Green;
+	Yellow;
+	Blue;
+	Magenta;
+	Cyan;
+	White;
+}
+
+enum DisplayAttribute
+{
+	Reset;        // normal
+	Bold;
+	Faint;        // not widely supported
+	Italic;       // not widely supported
+	Underline;
+	Blink;
+	BlinkRapid;   // not widely supported
+	Negative;
+	Conceal;      // not widely supported
+	CrossedOut;   // not widely supported
+}
+
+class ConsoleOut
+{
+	private static inline var CONSOLE_COMMAND_START:String = "\x1B[";
+
+	private static inline var COMMAND_RESET = "0";
+	private static inline var COMMAND_FOREGROUND_COLOR = "3";
+	private static inline var COMMAND_BACKGROUND_COLOR = "4";
+
+	private static var colorize:Bool;
+
+	public static function __init__()
+	{
+		// Neko Log.Out seems to have issues with utf8 on Windows
+		if (Sys.systemName() == "Windows")
+		{
+			colorize = false;
+		}
+		else
+		{
+			colorize = true;
+		}
+	}
+
+	public static function textFormatCodes(?foreground:Color, ?background:Color, ?attribute:DisplayAttribute):String
+	{
+		if (colorize)
+		{
+			var commands = new Array<String>();
+			if (attribute == null && foreground == null && background == null)
+			{
+				commands.push(COMMAND_RESET); // reset
+			}
+			else
+			{
+				if (attribute != null) commands.push(Std.string(Type.enumIndex(attribute)));
+				if (foreground != null) commands.push(COMMAND_FOREGROUND_COLOR + Type.enumIndex(foreground));
+				if (background != null) commands.push(COMMAND_BACKGROUND_COLOR + Type.enumIndex(background));
+			}
+			return CONSOLE_COMMAND_START + commands.join(";") + "m";
+		}
+		else
+		{
+			return "";
+		}
+	}
+
+  public static function print(message:String, ?foreground:Color, ?background:Color, ?attribute:DisplayAttribute)
+  {
+    Lib.println(textFormatCodes(foreground, background, attribute) + message + textFormatCodes());
+  }
+
+  public static function println(message:String, ?foreground:Color, ?background:Color, ?attribute:DisplayAttribute)
+  {
+    print(message + "\n", foreground, background, attribute);
+  }
+}
